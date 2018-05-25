f182256bd5b8155a317dd2c332f5d21e7b8cee0d
Bathmat <jkburch06@gmail.com>
2018-05-24 22:00:08 -0500

    Change to MinerCartel theme

--- app/globals.js
+++ app/globals.js
@@ -4,9 +4,9 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "Bathmat's Test Pool",
-		api_url : 'http://test.bathmatminingpools.com/api',
-		pool_server: "test.bathmatminingpools.com",
+		pool_name: "The MinerCartel Pool",
+		api_url : 'http://test.minercartel.com/api',
+		pool_server: "test.minercartel.com",
 		coin_abbr: "TEST",
 		api_refresh_interval: 30000,
 		app_update_interval: 10*60000
